28781d12ebd286e285396769823532ed7b982fe2
Danny Guinther <danny@zensight.co>
2016-12-02 08:51:45 -0500

    Update default version to 1.9.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['docker-compose']['config_directory'] = '/etc/compose.d'
-default['docker-compose']['version'] = '1.6.2'
+default['docker-compose']['version'] = '1.9.0'
 default['docker-compose']['include_docker'] = false
 default['docker-compose']['base_url'] = 'https://github.com/docker/compose/releases/download'
