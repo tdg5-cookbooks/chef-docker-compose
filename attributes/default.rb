7fbdc15d01658306c3744490369064c7d7a5240f
Danny Guinther <dguinther@seismic.com>
2018-01-08 14:35:50 -0500

    Update default docker-compose version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['docker-compose']['config_directory'] = '/etc/compose.d'
-default['docker-compose']['version'] = '1.10.0'
+default['docker-compose']['version'] = '1.18.0'
 default['docker-compose']['include_docker'] = false
 default['docker-compose']['base_url'] = 'https://github.com/docker/compose/releases/download'
